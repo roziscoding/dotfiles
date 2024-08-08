78864b750cd0d4fdfa9eb6e5038e6ce0893bedfd
Roz <rogerio.munhoz@lemon.energy>
2024-08-08 12:53:26 -0300

    feat: initial commit

---
+++ dot_envs.sh
@@ -0,0 +1,3 @@
+export PERSONAL_GITHUB_TOKEN={{ op://Private/Github Personal Token/token }}
+export PIPEDRIVE_CLIENTS_API_KEY={{ op://Lemon/Pipedrive/Chave API Clientes }}
+export PIPEDRIVE_LEADS_API_KEY={{ op://Lemon/Pipedrive/Chave API Leads }}
