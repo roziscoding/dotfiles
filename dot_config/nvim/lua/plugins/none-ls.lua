09b880b547f5328f8d29d2cd86091d31bebe330e
Roz <roz@rjmunhoz.me>
2024-08-12 12:29:31 -0300

    add linting and formatting

---
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,17 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.formatting.biome,
+			},
+		})
+	end,
+	keys = {
+		{ "<M-F>", vim.lsp.buf.format, desc = "NeoTree" },
+	},
+}
