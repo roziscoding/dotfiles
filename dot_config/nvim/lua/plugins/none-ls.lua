1fda5545d2fabd6aeaecc1c8ec4b8e110f39b9ec
Roz <roz@rjmunhoz.me>
2024-08-12 12:42:02 -0300

    ignore lazy lock

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -6,8 +6,6 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint,
-				null_ls.builtins.formatting.biome,
 			},
 		})
 	end,
