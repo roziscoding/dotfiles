766244de8bbcaffbb7deefcf1c0e05313373254c
Roz <roz@rjmunhoz.me>
2024-08-17 10:19:58 -0300

    add chezmoi integration to neovim

---
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,14 @@
+return {
+  "xvzc/chezmoi.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("chezmoi").setup({})
+
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+      callback = function()
+        vim.schedule(require("chezmoi.commands.__edit").watch)
+      end,
+    })
+  end,
+}
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,20 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	opts = {
-		defaults = {
-			file_ignore_patterns = {
-				"node_modules",
-				".git",
-			},
-		},
-	},
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "node_modules",
+        ".git",
+      },
+    },
+  },
+  config = function(opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+
+    telescope.load_extension("chezmoi")
+  end,
+  keys = {
+    { "<leader>cz", require("telescope").extensions.chezmoi.find_files, "Find files managed by chezmoi" },
+  },
 }
