87936c69d672e8eebd050bbd35f251fc1b1a2715
Roz <roz@rjmunhoz.me>
2024-08-25 13:54:16 -0300

    add chezmoi plugin config

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -2,7 +2,20 @@ return {
   "xvzc/chezmoi.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("chezmoi").setup({})
+    require("chezmoi").setup({
+      edit = {
+        watch = false,
+        force = false,
+      },
+      notification = {
+        on_open = true,
+        on_apply = true,
+        on_watch = true,
+      },
+      telescope = {
+        select = { "<CR>" },
+      },
+    })
 
     vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
       pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
