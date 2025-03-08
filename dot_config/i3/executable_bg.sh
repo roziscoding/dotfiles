c0402cb4d32c9639fdde58639848e9198c49baad
Roz <roz@rjmunhoz.me>
2025-03-08 12:42:10 -0300

    idk fix bg and stuff

--- dot_config/i3/executable_bg.sh
+++ dot_config/i3/executable_bg.sh
@@ -4,32 +4,29 @@
 # cron example:
 # 0 * * * * ~/.config/sway/scripts/bing_wallpaper.sh
 
-# exit on error
-set -e
-
 # set $SWAYSOCK if it's not set (for systemd or cron)
-if [ -z "$SWAYSOCK" ]; then
-  export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
-fi
+#if [ -z "$SWAYSOCK" ]; then
+#  export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
+#fi
 
 currentdate=$(date --iso)
 wlpath=${WALLPAPER_PATH:-"$HOME/Pictures/Wallpapers/$currentdate"}
-output=${WALLPAPER_OUTPUT:-"*"}
+#output=${WALLPAPER_OUTPUT:-"*"}
 baseurl="https://bing.biturl.top?format=image&resolution=UHD&mkt=en-US"
 
-echo $wlpath
+echo "$wlpath"
 
-mkdir -p $wlpath
+mkdir -p "$wlpath"
 
-echo $wlpath/regular.jpg
-https $baseurl -d --output $wlpath/regular.jpg
+echo "$wlpath"/regular.jpg
+https "$baseurl" -d --output "$wlpath"/regular.jpg
 
-killall swaybg || true
+# killall swaybg || true
 
-swaymsg "output $output bg $wlpath/regular.jpg fill"
+# swaymsg "output $output bg $wlpath/regular.jpg fill"
+feh --bg-fill "$wlpath/regular.jpg"
 
-convert $wlpath/regular.jpg -filter Gaussian -blur 0x8 -level 10%,90%,0.5 $wlpath/lock.jpg
+magick "$wlpath"/regular.jpg -filter Gaussian -blur 0x8 -level 10%,90%,0.5 "$wlpath"/lock.jpg
 
-ln -sfn $wlpath/regular.png $HOME/Pictures/Wallpapers/regular.jpg
-ln -sfn $wlpath/lock.png $HOME/Pictures/Wallpapers/lock.jpg
-
+ln -sfn "$wlpath"/regular.png "$HOME"/Pictures/Wallpapers/regular.jpg
+ln -sfn "$wlpath"/lock.png "$HOME"/Pictures/Wallpapers/lock.jpg
