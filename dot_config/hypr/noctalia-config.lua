10c6b17974a0c85d2d80ef84bdb11bface3b7df1
Roz <roz@meistrari.com>
2026-07-03 18:36:03 +0200

    hyprland and noctalia config

---
+++ dot_config/hypr/noctalia-config.lua
@@ -0,0 +1,25 @@
+hl.config({
+  general = {
+    gaps_in = 5,
+    gaps_out = 10,
+  },
+
+  decoration = {
+    rounding = 20,
+    rounding_power = 2,
+
+    shadow = {
+      enabled = true,
+      range = 4,
+      render_power = 3,
+      color = 0xee1a1a1a,
+    },
+
+    blur = {
+      enabled = true,
+      size = 3,
+      passes = 2,
+      vibrancy = 0.1696,
+    },
+  },
+})
