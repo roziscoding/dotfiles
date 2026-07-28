c3d62b2a61e2dfcd8d298d04e987bb6d26b53921
Roz <rogerio.j.munhoz@gmail.com>
2026-07-28 22:30:26 +0200

    fix some stuff

--- dot_config/hypr/monitors.lua
+++ dot_config/hypr/monitors.lua
@@ -5,8 +5,13 @@ require("globals")
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-	output = "",
-	mode = "preferred",
+	output = "DP-2",
+	mode = "1920x1080@60.00Hz",
 	position = "auto",
 	scale = "auto",
+})
+
+hl.monitor({
+	output = "eDP-1",
+	disabled = true,
 })
