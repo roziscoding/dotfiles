6d187fc0666852829155ba28c08921ef76e8a9f0
Roz <rogerio.j.munhoz@gmail.com>
2026-08-07 08:55:34 +0200

    set LEDs to wallpaper color on boot

--- dot_config/hypr/autostart.lua
+++ dot_config/hypr/autostart.lua
@@ -10,5 +10,7 @@ require("globals")
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd("noctalia")
-	hl.exec_cmd("openrgb --mode direct --color FF2C00")
+	hl.exec_cmd(
+		"openrgb --mode direct --color \"$(cat $(find ~/.config/noctalia/palettes -name '*.json' -print -quit) | jq '.dark.mOnError' -r | tr -d \\#)\""
+	)
 end)
